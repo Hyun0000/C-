81decbc106169201cf1a0d56c02d15e9589675e4
Hyun0000 <79898454+Hyun0000@users.noreply.github.com>
2022-02-02 14:23:31 +0900

    for 반복문
    for 반복문

---
+++ 10_for.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	char a[10] = { 'a', 'b', 'c', 'd', 'e' };
+
+	for (int i = 0; i < 5; i++) {
+		cout << i << "¹øÂ° : " << a[i] << endl;
+	}
+
+	return 0;
+}
