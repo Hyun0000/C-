21268e551871edf520b804ccf0fe5f6e98c76d2c
Hyun0000 <79898454+Hyun0000@users.noreply.github.com>
2022-02-02 21:04:40 +0900

    Logical operators(&& / || / !)
    Logical operators(&& / || / !)

---
+++ 13_Logical operators(논리연산자).cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	// 논리 표현식 : 하나 이상의 조건을 검사할 때 사용
+	// C++에는 3개의 논리 연산자가 있다. : 논리합, 논리곱, 논리부정 연산자
+	
+	// 논리곱 : &&
+	// 논리합 : ||
+	// 논리부정 연산자 : !
+	// !(true) = false / !(false) = true
+
+	return 0;
+}
